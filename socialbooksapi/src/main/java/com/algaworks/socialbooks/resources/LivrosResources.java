dbe6bc08300a91f8763e4b356fbdebe653d0a510
RobervalFernandes07 <106821633+RobervalFernandes07@users.noreply.github.com>
2024-11-05 11:27:57 -0300

    Add files via upload

---
+++ socialbooksapi/src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -0,0 +1,28 @@
+package com.algaworks.socialbooks.resources;
+import java.util.List;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.socialbooks.domain.Livro;
+import com.algaworks.socialbooks.repository.LivrosRepository;
+
+@RestController
+public class LivrosResources {
+	
+	//Criando uma variavel
+	@Autowired
+	private LivrosRepository livrosRepository;
+	
+	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	
+	public List<Livro> listar() {
+		
+		return livrosRepository.findAll();
+	}
+	
+
+}
